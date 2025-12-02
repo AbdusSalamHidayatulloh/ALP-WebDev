c469ebab72a04c16b6ff00f07d4b90d1b9a03ac0
chris <christoperjoel1@gmail.com>
2025-12-02 09:53:34 +0700

    Add time stamps

--- database/migrations/2025_12_01_122353_board.php
+++ database/migrations/2025_12_01_122353_board.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('board', function (Blueprint $table) {
             $table->id('board_id');
             $table->string('board_name');
+            $table->timestamps();
         });
     }
 
