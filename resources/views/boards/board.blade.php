e283ae56ea312e989bd435eb3e75de2a05f2b5e8
chris <christoperjoel1@gmail.com>
2025-12-06 20:57:28 +0700

    Finish UI board

--- resources/views/boards/board.blade.php
+++ resources/views/boards/board.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.nav')
 @section('title', 'Make a board')
 @section('mainContent')
-<div class="board-layout" class="d-flex flex-column">
-    {{-- Side for Inbox --}}
-    <aside id="sidebar" class="d-flex w-25 flex-column p-3 border end bg-light">
+<div class="board-layout d-flex flex-row">
+
+    {{-- Sidebar --}}
+    <aside id="sidebar" class="p-3 border-end bg-light">
         <h5 class="mb-3">Inbox</h5>
 
         <div class="inbox-form mb-3 d-flex flex-row gap-1">
@@ -15,12 +16,13 @@
             No inbox
         </div>
     </aside>
-    {{-- For the kanban board --}}
-    <main id="boardContent" class="p-3">
-        <h1 class="mb-4">Board Name</h1>
 
-        {{-- kanban board goes here later --}}
+    {{-- Board --}}
+    <main id="boardContent" class="p-4">
+        <h1 class="mb-4">Board Name</h1>
         <p class="text-muted">Put your lists here...</p>
     </main>
+
 </div>
+
 @endsection
